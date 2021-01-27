4bfb4216f750d8334013a6260c648b73b8c4c7a0
ellbristow <elliott@glo.systems>
2021-01-27 16:46:30 +0100

    change version number

--- website_google_tag_manager/__manifest__.py
+++ website_google_tag_manager/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Google Tag Manager",
-    "version": "14.0.0.0.1",
+    "version": "14.0.1.0.0",
     "author": "ABF OSIELL, Tecnativa, Odoo Community Association (OCA), Glodo",
     "license": "AGPL-3",
     "application": False,
