f8bba3734f23ce6aed800c568a30c07a69b878bd
ellbristow <elliott@glo.systems>
2021-01-29 13:03:49 +0100

    Straight copy of v13, version iterated to 14.0

---
+++ website_typed_text/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Website Animated Text",
+    "category": "Website",
+    "version": "14.0.1.0.0",
+    "website": "https://github.com/OCA/website",
+    "author": "Onestein,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["website"],
+    "data": ["templates/assets.xml", "templates/snippets.xml"],
+    "installable": True,
+}
